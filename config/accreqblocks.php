d6700fd4de72b1eb12526760023a33cae269ec2d
Kenny2github <kenny2minecraft@gmail.com>
2020-09-14 04:33:06 +0200

    don't include config

--- config/accreqblocks.php
+++
@@ -1,6 +0,0 @@
-<?php
-//THIS IS THE LIST OF USERNAMES BLOCKED FROM SUBMITTING ACCOUNT REQUESTS
-//ADD USERNAMES AS SHOWN BELOW. THEY MUST BE ENTIRELY LOWERCASE AND HAVE UNDERSCORES REPLACED WITH SPACES.
-$acc_req_blocked_usernames = array();
-$acc_req_blocked_usernames[] = 'world 3';
-$acc_req_blocked_usernames[] = 'mrec'; //blocked for using a disposable email address (purity.annalysse@itis0k.com)
